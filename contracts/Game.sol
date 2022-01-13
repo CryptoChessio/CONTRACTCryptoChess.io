55144deb164daa700a8a29f63327eb0455770eed
theArctesian <stephen.d.okita@gmail.com>
2022-01-14 07:49:04 +0800

    some money shit

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -11,19 +11,59 @@ contract Game{
     uint teamCount;
     Player[] Players;
 
-    mapping(address => uint) public user;
+    function Game(uint _teamCount) {
+        teamCount = _teamCount;
+    }
 
+    function join(uint _team, uint _userAddress) {
+        if (teamCount == 0) {
+            throw;
+        }
+        if (_team > teamCount) {
+            throw;
+        }
+        if (Players[_team].userAddress != 0) {
+            throw;
+        }
+        Players[_team].team = _team;
+        Players[_team].userAddress = _userAddress;
+    }
+
+    function getTeam(uint _userAddress) constant returns (uint) {
+        for (uint i = 0; i < teamCount; i++) {
+            if (Players[i].userAddress == _userAddress) {
+                return Players[i].team;
+            }
+        }
+        return 0;
+    }
+
+    function getUserAddress(uint _team) constant returns (uint) {
+        return Players[_team].userAddress;
+    }
+
+    function getTeamCount() constant returns (uint) {
+        return teamCount;
+    }
+
+    function getPlayerCount() constant returns (uint) {
+        uint count = 0;
+        for (uint i = 0; i < teamCount; i++) {
+            if (Players[i].userAddress != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    function getPlayer(uint _team) constant returns (uint) {
+        return Players[_team].userAddress;
+    }
+    
     function addPlayer() public{
         Player memory player = Players[teamCount];
         Player.userAddress = msg.sender;
         Player.team = teamCount;
-        incTeamCount();
     }
 
-    function incTeamCount() public{
-        if(teamCount = 1)
-            teamCount = 0;
-        if(teamCount < 1)
-            teamCount +=1;
-    }
 }
